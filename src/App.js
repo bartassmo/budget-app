9ff8fc5e241353cf85b1aeacaf7e363653ffb437
Bartłomiej Smołucha <bartlomiej@smolucha.pl>
2023-12-29 23:14:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Kontroluj swoje domowe wydatki.</h1>
+        <button className="App-btn">Get Started</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
